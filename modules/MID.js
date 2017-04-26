9673cb2726f1d8566d1bb2a4d13a6939a23cb761
kdm <kennygprs@gmail.com>
2017-04-25 22:35:17 -0400

    Update

--- modules/MID.js
+++ modules/MID.js
@@ -1,16 +1,5 @@
 var module_name = __filename.slice(__dirname.length + 1, -3);
 
-// Pad string for IKE text screen length (20 characters)
-String.prototype.mid_pad = function() {
-	var string = this;
-
-	while (string.length < 20) {
-		string = string + ' ';
-	}
-
-	return string;
-}
-
 // Interval var
 var interval_status_loop;
 
@@ -35,12 +24,10 @@ function ascii2hex(str) {
 
 function text(message) {
 	console.log('[node::MID] Sending text to MID screen: \'%s\'', message);
-	message = message.mid_pad();
 
 	// Need to center text..
 	var message_hex = [0x23, 0x00, 0x20];
-	var message_hex = message_hex.concat(ascii2hex(message));
-	// var message_hex = message_hex.concat(0x04);
+	var message_hex = message_hex.concat(ascii2hex(pad(message, 20)));
 
 	bus_client.data_send({
 		src: 'IKE',
@@ -146,93 +133,104 @@ function parse_out(data) {
 	switch (data.msg[0]) {
 		case 0x20: // Broadcast: display status
 			data.command = 'bro';
-			data.value   = 'display status';
+			data.value   = 'display status: ';
 			break;
 
 		case 0x31: // Broadcast: button pressed
 			data.command = 'bro';
-			data.value   = 'button pressed';
+			data.value   = 'button pressed: '+data.msg[1]+' '+data.msg[2]+' '+data.msg[3];
 
-			// 31 00 00 01,MID,RAD,Button Button_1_pressed
-			// 31 00 00 03,MID,RAD,Button Button_3_pressed
-			// 31 00 00 03,MID,RAD,Button TP_MUTE_pressed
-			// 31 00 00 04,MID,RAD,Button Button_4_pressed
-			// 31 00 00 04,MID,RAD,Button VOLUME_+_pressed
-			// 31 00 00 05,MID,RAD,Button Button_5_pressed
-			// 31 00 00 06,MID,RAD,Button Button_6_pressed
-			// 31 00 00 06,MID,RAD,Button SEEK_<_pressed
-			// 31 00 00 07,MID,RAD,Button SEEK_>_pressed
-			// 31 00 00 08,MID,RAD,Button CC/CD_pressed
-			// 31 00 00 09,MID,RAD,Button FM_pressed
-			// 31 00 00 0A,MID,RAD,Button Button_A_pressed
-			// 31 00 00 0B,MID,RAD,Button Button_B_pressed
-			// 31 00 00 0B,MID,RAD,Button TONE_pressed
-			// 31 00 00 0C,MID,RAD,Button MAN_pressed
-			// 31 00 00 0D,MID,RAD,Button Button_D_pressed
-			// 31 00 00 0D,MID,RAD,Button MEM_pressed
-			// 31 00 00 0E,MID,RAD,Button SC/RP_pressed
-			// 31 00 00 23,MID,RAD,Button Button_3_pressed_long
-			// 31 00 00 25,MID,RAD,Button Button_5_pressed_long
-			// 31 00 00 2B,MID,RAD,Button Button_B_pressed_long
-			// 31 00 00 2D,MID,RAD,Button Button_D_pressed_long
-			// 31 00 00 41,MID,RAD,Button Button_1_released
-			// 31 00 00 43,MID,RAD,Button Button_3_released
-			// 31 00 00 43,MID,RAD,Button TP_MUTE_released
-			// 31 00 00 44,MID,RAD,Button Button_4_released
-			// 31 00 00 44,MID,RAD,Button VOLUME_+_released
-			// 31 00 00 45,MID,RAD,Button Button_5_released
-			// 31 00 00 46,MID,RAD,Button Button_6_released
-			// 31 00 00 46,MID,RAD,Button SEEK_<_released
-			// 31 00 00 47,MID,RAD,Button SEEK_>_released
-			// 31 00 00 48,MID,RAD,Button CC/CD_released
-			// 31 00 00 49,MID,RAD,Button FM_released
-			// 31 00 00 4A,MID,RAD,Button Button_A_released
-			// 31 00 00 4B,MID,RAD,Button Button_B_released
-			// 31 00 00 4B,MID,RAD,Button TONE_released
-			// 31 00 00 4C,MID,RAD,Button MAN_released
-			// 31 00 00 4D,MID,RAD,Button Button_D_released
-			// 31 00 00 4D,MID,RAD,Button MEM_released
-			// 31 00 00 4E,MID,RAD,Button SC/RP_released
-			// 31 00 10 06,MID,RAD,Button Button_6_pressed
-			// 31 00 10 0B,MID,RAD,Button Button_B_pressed
-			// 31 00 10 46,MID,RAD,Button Button_6_released
-			// 31 00 10 4B,MID,RAD,Button Button_B_released
-			// 31 00 15 07,MID,RAD,Button Button_7_pressed
-			// 31 00 15 0B,MID,RAD,Button Button_B_pressed
-			// 31 00 15 47,MID,RAD,Button Button_7_released
-			// 31 00 15 4B,MID,RAD,Button Button_B_released
-			// 31 00 21 06,MID,RAD,Button Button_6_pressed
-			// 31 00 21 46,MID,RAD,Button Button_6_released
-			// 31 00 33 04,MID,RAD,Button Button_4_pressed
-			// 31 00 33 06,MID,RAD,Button Button_6_pressed
-			// 31 00 33 09,MID,RAD,Button Button_9_pressed
-			// 31 00 33 0A,MID,RAD,Button Button_A_pressed
-			// 31 00 33 0B,MID,RAD,Button Button_B_pressed
-			// 31 00 33 44,MID,RAD,Button Button_4_released
-			// 31 00 33 46,MID,RAD,Button Button_6_released
-			// 31 00 33 49,MID,RAD,Button Button_9_released
-			// 31 00 33 4A,MID,RAD,Button Button_A_released
-			// 31 00 33 4B,MID,RAD,Button Button_B_released
-			// 31 00 34 02,MID,RAD,Button Button_2_pressed
-			// 31 00 34 04,MID,RAD,Button Button_4_pressed
-			// 31 00 34 08,MID,RAD,Button Button_8_pressed
-			// 31 00 34 0B,MID,RAD,Button Button_B_pressed
-			// 31 00 34 2B,MID,RAD,Button Button_B_pressed_long
-			// 31 00 34 42,MID,RAD,Button Button_2_released
-			// 31 00 34 44,MID,RAD,Button Button_4_released
-			// 31 00 34 48,MID,RAD,Button Button_8_released
-			// 31 00 34 4B,MID,RAD,Button Button_B_released
-			// 31 00 35 03,MID,RAD,Button Button_3_pressed
-			// 31 00 35 43,MID,RAD,Button Button_3_released
-			// 31 00 46 06,MID,RAD,Button Button_6_pressed
-			// 31 00 46 46,MID,RAD,Button Button_6_released
-			// 31 01 20 00,MID,IKE,Button Button_0_pressed
-			// 31 01 20 40,MID,IKE,Button Button_0_released
+			// 00 00 01,MID,RAD,Button Button_1_pressed
+			// 00 00 03,MID,RAD,Button Button_3_pressed
+			// 00 00 03,MID,RAD,Button TP_MUTE_pressed
+			// 00 00 04,MID,RAD,Button Button_4_pressed
+			// 00 00 04,MID,RAD,Button VOLUME_+_pressed
+			// 00 00 05,MID,RAD,Button Button_5_pressed
+			// 00 00 06,MID,RAD,Button Button_6_pressed
+			// 00 00 06,MID,RAD,Button SEEK_<_pressed
+			// 00 00 07,MID,RAD,Button SEEK_>_pressed
+			// 00 00 08,MID,RAD,Button CC/CD_pressed
+			// 00 00 09,MID,RAD,Button FM_pressed
+			// 00 00 0A,MID,RAD,Button Button_A_pressed
+			// 00 00 0B,MID,RAD,Button Button_B_pressed
+			// 00 00 0B,MID,RAD,Button TONE_pressed
+			// 00 00 0C,MID,RAD,Button MAN_pressed
+			// 00 00 0D,MID,RAD,Button Button_D_pressed
+			// 00 00 0D,MID,RAD,Button MEM_pressed
+			// 00 00 0E,MID,RAD,Button SC/RP_pressed
+			// 00 00 23,MID,RAD,Button Button_3_pressed_long
+			// 00 00 25,MID,RAD,Button Button_5_pressed_long
+			// 00 00 2B,MID,RAD,Button Button_B_pressed_long
+			// 00 00 2D,MID,RAD,Button Button_D_pressed_long
+			// 00 00 41,MID,RAD,Button Button_1_released
+			// 00 00 43,MID,RAD,Button Button_3_released
+			// 00 00 43,MID,RAD,Button TP_MUTE_released
+			// 00 00 44,MID,RAD,Button Button_4_released
+			// 00 00 44,MID,RAD,Button VOLUME_+_released
+			// 00 00 45,MID,RAD,Button Button_5_released
+			// 00 00 46,MID,RAD,Button Button_6_released
+			// 00 00 46,MID,RAD,Button SEEK_<_released
+			// 00 00 47,MID,RAD,Button SEEK_>_released
+			// 00 00 48,MID,RAD,Button CC/CD_released
+			// 00 00 49,MID,RAD,Button FM_released
+			// 00 00 4A,MID,RAD,Button Button_A_released
+			// 00 00 4B,MID,RAD,Button Button_B_released
+			// 00 00 4B,MID,RAD,Button TONE_released
+			// 00 00 4C,MID,RAD,Button MAN_released
+			// 00 00 4D,MID,RAD,Button Button_D_released
+			// 00 00 4D,MID,RAD,Button MEM_released
+			// 00 00 4E,MID,RAD,Button SC/RP_released
+			// 00 10 06,MID,RAD,Button Button_6_pressed
+			// 00 10 0B,MID,RAD,Button Button_B_pressed
+			// 00 10 46,MID,RAD,Button Button_6_released
+			// 00 10 4B,MID,RAD,Button Button_B_released
+			// 00 15 07,MID,RAD,Button Button_7_pressed
+			// 00 15 0B,MID,RAD,Button Button_B_pressed
+			// 00 15 47,MID,RAD,Button Button_7_released
+			// 00 15 4B,MID,RAD,Button Button_B_released
+			// 00 21 06,MID,RAD,Button Button_6_pressed
+			// 00 21 46,MID,RAD,Button Button_6_released
+			// 00 33 04,MID,RAD,Button Button_4_pressed
+			// 00 33 06,MID,RAD,Button Button_6_pressed
+			// 00 33 09,MID,RAD,Button Button_9_pressed
+			// 00 33 0A,MID,RAD,Button Button_A_pressed
+			// 00 33 0B,MID,RAD,Button Button_B_pressed
+			// 00 33 44,MID,RAD,Button Button_4_released
+			// 00 33 46,MID,RAD,Button Button_6_released
+			// 00 33 49,MID,RAD,Button Button_9_released
+			// 00 33 4A,MID,RAD,Button Button_A_released
+			// 00 33 4B,MID,RAD,Button Button_B_released
+			// 00 34 02,MID,RAD,Button Button_2_pressed
+			// 00 34 04,MID,RAD,Button Button_4_pressed
+			// 00 34 08,MID,RAD,Button Button_8_pressed
+			// 00 34 0B,MID,RAD,Button Button_B_pressed
+			// 00 34 2B,MID,RAD,Button Button_B_pressed_long
+			// 00 34 42,MID,RAD,Button Button_2_released
+			// 00 34 44,MID,RAD,Button Button_4_released
+			// 00 34 48,MID,RAD,Button Button_8_released
+			// 00 34 4B,MID,RAD,Button Button_B_released
+			// 00 35 03,MID,RAD,Button Button_3_pressed
+			// 00 35 43,MID,RAD,Button Button_3_released
+			// 00 46 06,MID,RAD,Button Button_6_pressed
+			// 00 46 46,MID,RAD,Button Button_6_released
+			// 01 20 00,MID,IKE,Button Button_0_pressed
+			// 01 20 40,MID,IKE,Button Button_0_released
 			break;
 
-		case 0x32: // Broadcast: volume control
-			data.command = 'bro';
-			data.value   = 'volume control';
+		case 0x32: // Broadcast: Volume control
+			data.command = 'con';
+
+			// Determine volume change direction
+			if (bitmask.bit_test(data.msg[1], 0x01)) {
+				var direction = '+';
+				data.msg = data.msg-1;
+			}
+			else {
+				var direction = '-';
+			}
+
+			data.value = 'volume '+direction+(data.msg[1]/10);
+
 			// data.msg[1] -
 			// -1 : 10
 			// -2 : 20
