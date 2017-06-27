172d019083784c4094d79d246bc579a6e5b8beb6
kdm <kennygprs@gmail.com>
2017-06-27 17:49:17 -0400

    Update

---
+++ modules/CON.js
@@ -0,0 +1,16 @@
+var module_name = __filename.slice(__dirname.length + 1, -3);
+
+// Parse data sent from module
+function parse_out(data) {
+	switch (data.msg[0]) {
+		default:
+			data.command = 'unk';
+			data.value   = Buffer.from(data.msg);
+	}
+
+	log.bus(data);
+}
+
+module.exports = {
+	parse_out : (data) => { parse_out(data); },
+};
