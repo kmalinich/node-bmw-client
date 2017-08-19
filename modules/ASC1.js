a74d7bc1c4e215458052956d7d1a8f528ba1205c
kdm <kennygprs@gmail.com>
2017-08-19 18:45:09 -0400

    Update to include parsing wheel speeds and steering angle

--- modules/ASC1.js
+++ modules/ASC1.js
@@ -1,5 +1,50 @@
 const module_name = __filename.slice(__dirname.length + 1, -3);
 
+function parse_1f0(data) {
+	let wheel_speed = {
+		front : {
+			left  : (data.msg[0]+parseInt('0x'+data.msg[1].toString(16).slice(-1))*256)/16,
+			right : (data.msg[2]+parseInt('0x'+data.msg[3].toString(16).slice(-1))*256)/16,
+		},
+		rear : {
+			left  : (data.msg[4]+parseInt('0x'+data.msg[5].toString(16).slice(-1))*256)/16,
+			right : (data.msg[6]+parseInt('0x'+data.msg[7].toString(16).slice(-1))*256)/16,
+		},
+	};
+
+	update.status('vehicle.wheel_speed.front.left',  wheel_speed.front.left);
+	update.status('vehicle.wheel_speed.front.right', wheel_speed.front.right);
+	update.status('vehicle.wheel_speed.rear.left',   wheel_speed.rear.left);
+	update.status('vehicle.wheel_speed.rear.right',  wheel_speed.rear.right);
+}
+
+function parse_1f5(data) {
+	let angle = 0;
+	if (data.msg[1] > 127) {
+		angle = -1*(((data.msg[1]-128)*256)+data.msg[0]);
+	}
+	else {
+		angle = (data.msg[1]*256)+data.msg[0];
+	}
+
+	let velocity = 0;
+	if (data.msg[3] > 127) {
+		velocity = -1*(((data.msg[3]-128)*256)+data.msg[2]);
+	}
+	else {
+		velocity = (data.msg[3]*256)+data.msg[2];
+	}
+
+	let steering = {
+		angle    : angle*0.045,
+		velocity : velocity*0.045,
+	};
+
+	update.status('vehicle.steering.angle', steering.angle);
+	update.status('vehicle.steering.velocity', steering.angle);
+}
+
+
 // Parse data sent from module
 function parse_out(data) {
 	data.command = 'bro';
@@ -10,6 +55,7 @@ function parse_out(data) {
 			break;
 
 		case 0x1F0:
+			parse_1f0(data);
 			data.value = 'Wheel speeds';
 			break;
 
@@ -18,6 +64,7 @@ function parse_out(data) {
 			break;
 
 		case 0x1F5:
+			parse_1f5(data);
 			data.value = 'Steering angle';
 			break;
 
