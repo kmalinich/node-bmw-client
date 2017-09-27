2775a10bceb1f66baa601a55a2fae705311ed2d8
kdm <kennygprs@gmail.com>
2017-09-26 23:48:16 -0400

    Update

--- modules/RAD.js
+++ modules/RAD.js
@@ -12,6 +12,7 @@ function decode_audio_control_command(data) {
 	// 0xE0 - dsp1+
 
 	let mask = bitmask.check(data.msg[1]).mask;
+	console.dir(mask, { colors : true });
 
 	// Bounce if bit8 (no bits set) is true
 	if (mask.bit8) {
