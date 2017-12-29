656af6155c90fceec81efd6bf393c76db700681b
kdm <kennygprs@gmail.com>
2017-12-29 16:57:55 -0500

    Update

--- bluez-test2.js
+++ bluez-test2.js
@@ -6,42 +6,42 @@ const Bluez     = require('bluez');
 const bluetooth = new Bluez();
 
 
-function event_log(action, type, properties) {
+function event_log(action, type, data) {
 	console.log('\n======== Event : %s (%s) ========', type, action);
-	console.log(objfmt(properties));
+	console.log(objfmt(data));
 	console.log('======== Event : %s (%s) ========\n', type, action);
 }
 
 // Register callbacks for changed properties
-bluetooth.on('changed-Adapter',        async (properties) => { event_log('Changed', 'Adapter',        properties); });
-bluetooth.on('changed-Device',         async (properties) => { event_log('Changed', 'Device',         properties); });
-bluetooth.on('changed-Filesystem',     async (properties) => { event_log('Changed', 'Filesystem',     properties); });
-bluetooth.on('changed-MediaControl',   async (properties) => { event_log('Changed', 'MediaControl',   properties); });
-bluetooth.on('changed-MediaItem',      async (properties) => { event_log('Changed', 'MediaItem',      properties); });
-bluetooth.on('changed-MediaPlayer',    async (properties) => { event_log('Changed', 'MediaPlayer',    properties); });
-bluetooth.on('changed-MediaTransport', async (properties) => { event_log('Changed', 'MediaTransport', properties); });
-bluetooth.on('changed-Network',        async (properties) => { event_log('Changed', 'Network',        properties); });
+bluetooth.on('changed-Adapter',        async (data) => { event_log('Changed', 'Adapter',        data); });
+bluetooth.on('changed-Device',         async (data) => { event_log('Changed', 'Device',         data); });
+bluetooth.on('changed-Filesystem',     async (data) => { event_log('Changed', 'Filesystem',     data); });
+bluetooth.on('changed-MediaControl',   async (data) => { event_log('Changed', 'MediaControl',   data); });
+bluetooth.on('changed-MediaItem',      async (data) => { event_log('Changed', 'MediaItem',      data); });
+bluetooth.on('changed-MediaPlayer',    async (data) => { event_log('Changed', 'MediaPlayer',    data); });
+bluetooth.on('changed-MediaTransport', async (data) => { event_log('Changed', 'MediaTransport', data); });
+bluetooth.on('changed-Network',        async (data) => { event_log('Changed', 'Network',        data); });
 
 // Register callbacks for new interfaces
-bluetooth.on('added-Adapter',        async (properties) => { event_log('Added', 'Adapter',        properties); });
-bluetooth.on('added-Filesystem',     async (properties) => { event_log('Added', 'Filesystem',     properties); });
-bluetooth.on('added-MediaControl',   async (properties) => { event_log('Added', 'MediaControl',   properties); });
-bluetooth.on('added-MediaItem',      async (properties) => { event_log('Added', 'MediaItem',      properties); });
-bluetooth.on('added-MediaPlayer',    async (properties) => { event_log('Added', 'MediaPlayer',    properties); });
-bluetooth.on('added-MediaTransport', async (properties) => { event_log('Added', 'MediaTransport', properties); });
-bluetooth.on('added-Network',        async (properties) => { event_log('Added', 'Network',        properties); });
+bluetooth.on('added-Adapter',        async (data) => { event_log('Added', 'Adapter',        data); });
+bluetooth.on('added-Filesystem',     async (data) => { event_log('Added', 'Filesystem',     data); });
+bluetooth.on('added-MediaControl',   async (data) => { event_log('Added', 'MediaControl',   data); });
+bluetooth.on('added-MediaItem',      async (data) => { event_log('Added', 'MediaItem',      data); });
+bluetooth.on('added-MediaPlayer',    async (data) => { event_log('Added', 'MediaPlayer',    data); });
+bluetooth.on('added-MediaTransport', async (data) => { event_log('Added', 'MediaTransport', data); });
+bluetooth.on('added-Network',        async (data) => { event_log('Added', 'Network',        data); });
 
-bluetooth.on('added-Device', async (properties) => {
-	event_log('Added', 'Device', properties);
+bluetooth.on('added-Device', async (data) => {
+	event_log('Added', 'Device', data);
 
-	event_log('DeviceInterface', 'Log', 'Attempting to get interface of device with address ' + properties.Address);
-	let device = await bluetooth.getDevice(properties.Address);
+	event_log('DeviceInterface', 'Log', 'Attempting to get interface of device with address ' + data.properties.Address);
+	let device = await bluetooth.getDevice(data.properties.Address);
 
-	if (properties.Connected === false) {
-		event_log('DeviceConnect', 'Log', 'Attempting to connect to device with address ' + properties.Address);
+	if (data.Connected === false) {
+		event_log('DeviceConnect', 'Log', 'Attempting to connect to device with address ' + data.properties.Address);
 
 		await device.Connect().catch((err) => {
-			event_log('DeviceConnect', 'Error', 'Error while attempting to connect to device with address ' + properties.Address + ': ' + err.message);
+			event_log('DeviceConnect', 'Error', 'Error while attempting to connect to device with address ' + data.properties.Address + ': ' + err.message);
 		});
 	}
 });
