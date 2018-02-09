e099ed0f279364efa6f2f2aa6eb141575aa803dd
kdm <kennygprs@gmail.com>
2018-02-08 19:45:59 -0500

    Update

--- modules/PDC.js
+++ modules/PDC.js
@@ -1,9 +1,22 @@
+function decode_gong_status(data) {
+	data.command = 'bro';
+	data.value   = 'TODO gong status ' + data.msg;
+
+	return data;
+}
+
 // Parse data sent from module
 function parse_out(data) {
 	switch (data.msg[0]) {
-		default:
+		case 0x07 : { // Gong status
+			data = decode_gong_status(data);
+			break;
+		}
+
+		default : {
 			data.command = 'unk';
 			data.value   = Buffer.from(data.msg);
+		}
 	}
 
 	log.bus(data);
