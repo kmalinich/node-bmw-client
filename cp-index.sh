a6de052d8bef7ec0288d7af1f10fac766fa34c9c
kdm <kennygprs@gmail.com>
2018-04-25 10:42:26 -0400

    Update

--- cp-index.sh
+++ cp-index.sh
@@ -4,16 +4,17 @@ cd node_modules || exit 1
 
 for FILE_V8 in $(find . -name index-v8.js); do
 	FILE_V9="${FILE_V8/v8/v9}"
-	if [[ ! -s "${FILE_V9}" ]]; then
-		cp -fv "${FILE_V8}" "${FILE_V9}" || exit 1
-	fi
+	FILE_V10="${FILE_V8/v8/v10}"
+
+	[[ ! -s "${FILE_V9}"  ]] && cp -fv "${FILE_V8}" "${FILE_V9}"
+	[[ ! -s "${FILE_V10}" ]] && cp -fv "${FILE_V8}" "${FILE_V10}"
 done
 
-cd .. || exit 2
+cd .. || exit 1
 
 if grep -q pi /etc/passwd > /dev/null 2>&1; then
 	echo "Fixing permissions (pi:pi)"
-	chown -R pi:pi .
+	chown -R pi: .
 fi
 
 exit 0
