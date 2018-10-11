527093532c91aab794aa4c5eb3a576abc9f830cc
kdm <kennygprs@gmail.com>
2018-10-11 02:44:34 -0400

    Update

--- modules/CAS1.js
+++ modules/CAS1.js
@@ -54,29 +54,33 @@ function decode_ignition(data) {
 						});
 
 						// Tach
-						bus.data.send({
-							bus  : config.kombi.can_intf,
-							id   : 0x6F1,
-							data : Buffer.from([ 0x60, 0x05, 0x30, 0x21, 0x06, 0x12, 0x0E, 0xFF ]),
-						});
-
 						setTimeout(() => {
-							log.module('Executing KOMBI gauge reset');
-
-							// Reset speedo
 							bus.data.send({
 								bus  : config.kombi.can_intf,
 								id   : 0x6F1,
-								data : Buffer.from([ 0x60, 0x03, 0x30, 0x20, 0x00, 0xFF, 0xFF, 0xFF ]),
+								data : Buffer.from([ 0x60, 0x05, 0x30, 0x21, 0x06, 0x12, 0x0E, 0xFF ]),
 							});
 
-							// Reset tach
-							bus.data.send({
-								bus  : config.kombi.can_intf,
-								id   : 0x6F1,
-								data : Buffer.from([ 0x60, 0x03, 0x30, 0x21, 0x00, 0xFF, 0xFF, 0xFF ]),
-							});
-						}, 1500);
+							setTimeout(() => {
+								log.module('Executing KOMBI gauge reset');
+
+								// Reset speedo
+								bus.data.send({
+									bus  : config.kombi.can_intf,
+									id   : 0x6F1,
+									data : Buffer.from([ 0x60, 0x03, 0x30, 0x20, 0x00, 0xFF, 0xFF, 0xFF ]),
+								});
+
+								// Reset tach
+								setTimeout(() => {
+									bus.data.send({
+										bus  : config.kombi.can_intf,
+										id   : 0x6F1,
+										data : Buffer.from([ 0x60, 0x03, 0x30, 0x21, 0x00, 0xFF, 0xFF, 0xFF ]),
+									});
+								}, 100);
+							}, 1500);
+						}, 100);
 					}, 500);
 				}
 				break;
