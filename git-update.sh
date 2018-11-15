be1157f96cfc8e5b2512716de0764354801b4e44
root <root@kdm-e39-01>
2018-11-15 11:30:17 -0500

    Update

--- git-update.sh
+++ git-update.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/env bash
 
+_out() {
+	local BAR="==========="
+
+	echo
+	echo "-${BAR} ${1} ${BAR}-"
+}
+
 _smf() {
 	git submodule foreach --recursive
 }
 
+_out "Repo fetch"
 git fetch --all --prune
-git pull --rebase --prune
 
+_out "Repo pull"
+git pull --prune
+
+_out "Submodule fetch"
 _smf fetch --all --prune
+
+_out "Submodule checkout master"
 _smf checkout master
-_smf pull --rebase --prune
+
+_out "Submodule pull"
+_smf pull --prune
+
+_out "Submodule status"
+git submodule
+
+_out "Repo status"
+git status
