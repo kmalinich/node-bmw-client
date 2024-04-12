80284203b5ea8ac36e2959207ccc8b98587964b4
kdm <kennygprs@gmail.com>
2024-04-12 12:27:44 -0400

    Update

--- modules/CCM.js
+++ modules/CCM.js
@@ -1,7 +1,10 @@
 // Broadcast: check control message
 function parse_cc_message(data) {
 	data.command = 'bro';
-	data.value   = 'TODO: check control message';
+
+	const checkControlMessage = data.msg.slice(3, 20);
+
+	data.value = `TODO: check control message - values: ${hex.i2s(data.msg[1])} ${hex.i2s(data.msg[2])} - message: ${checkControlMessage}`;
 	console.dir({ msg : data.msg });
 
 	return data;
@@ -17,7 +20,7 @@ function parse_cc_sensors(data) {
 		case 0x00 : data.value += 'none';                  break;
 		case 0x04 : data.value += 'key in ignition';       break;
 		case 0x12 : data.value += 'seatbelt not fastened'; break;
-		default   : data.value += data.msg[1];
+		default   : data.value += hex.i2s(data.msg[1]);
 	}
 
 	return data;
